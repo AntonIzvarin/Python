1251c148246382faf5ad25fd3d5515ddfab398b9
Your Name <you@example.com>
2026-09-20 12:17:08 +0300

    Домашняя работа lesson_06

--- lesson_06/test_lesson06_task2.py
+++ lesson_06/test_lesson06_task2.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_session_storage_auth():
     driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)
+
     driver.get("https://gitflic.ru/")
     # Добавляем cookie с токеном авторизации
     driver.add_cookie(
@@ -22,14 +25,16 @@ def test_session_storage_auth():
     # Теперь мы авторизованы!
     # Можем сразу перейти в личный кабинет
     driver.get("https://gitflic.ru/user/anton180188")
-    sleep(2)
+
+    wait.until(EC.url_contains("anton180188"))
 
     url_1 = driver.current_url
     # Удаляем все cookies (выходим из аккаунта)
     driver.delete_all_cookies()
     # Обновляем страницу
     driver.refresh()
-    sleep(2)
+
+    wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
 
     # Добавляем cookie с токеном авторизации
     driver.add_cookie(
@@ -48,16 +53,17 @@ def test_session_storage_auth():
     # Теперь мы авторизованы!
     # Можем сразу перейти в личный кабинет
     driver.get("https://gitflic.ru/user/qwerty180188")
-    sleep(2)
+
+    wait.until(EC.url_contains("qwerty180188"))
 
     url_2 = driver.current_url
     # Удаляем все cookies (выходим из аккаунта)
     driver.delete_all_cookies()
     # Обновляем страницу
     driver.refresh()
-    sleep(2)
 
-    assert url_1 != url_2, \
-        f"Ожидалось, что URL будут разными, но оба равны: {url_1}"
+    wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
+
+    assert url_1 != url_2, f"Ожидалось, что URL будут разными, но оба равны: {url_1}"
 
     driver.quit()
